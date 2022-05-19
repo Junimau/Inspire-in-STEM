0c7d26e95c7900435dfacad5fd99718862cdcb57
Junimau <105627971+Junimau@users.noreply.github.com>
2022-05-19 18:52:16 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+#phython basics
+
+#this is a comment
+
+
+#adding two numbers a and b
+
+#this is for a
+#multi/many lines
+#comment
+
+print ("my name is june")
+#print
---
+++ lesson5.py
@@ -0,0 +1,15 @@
+#integers
+
+
+x=100
+y=200
+
+sum_of=x+y
+prod=x*y
+diff=y-x
+quot=x/y
+
+print("the sum of x and y is "+str(sum_of))
+print("the product of x and y is"+str(prod))
+print("the difference between y and x is"+str(diff))
+print("the quotient of x and y is"+str(quot))
