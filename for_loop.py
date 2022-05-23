76622ecbc3a8c68fc993ad2a01cba3d672770d08
Junimau <105627971+Junimau@users.noreply.github.com>
2022-05-23 14:15:06 +0300

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,33 @@
+#!usr/bin/python
+
+##################
+#for_loop
+#Name :Maureen june
+#Date :23/5/2022
+
+################
+print("number\tsquare")
+print("==============")
+for number in range(0,9):
+    print(number)
+
+#print a pyramid of stars 
+
+print("\t\t\t*\t\t\t")
+print("\t\t*\t*\t*\t")
+print("\t*\t*\t*\t*\t*\t")
+print("*\t*\t*\t*\t*\t*\t*\t")
+
+#print a diamond of stars 
+
+print("\t*\t*\t*\t*\t*\t")
+print("\t\t*\t*\t*\t")
+print("\t\t\t*\t\t\t")
+
+
+
+
+
+    
+
+
