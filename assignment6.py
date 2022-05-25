21c7583a5c56f7f53fa3922adb1897855aaece05
Junimau <105627971+Junimau@users.noreply.github.com>
2022-05-25 18:02:06 +0300

    Add files via upload

---
+++ assignment6.py
@@ -0,0 +1,20 @@
+colours=["red","green","blue","purple"]
+#print each colour in capital
+
+i =1
+while i < len(colours):
+    if(colours[1]=='green'):
+        print(colours[1].upper())
+        i +=1
+
+i =2
+while i < len(colours):
+    if(colours[2]=='blue'):
+       print(colours[2].upper())
+       i +=1
+
+i =3
+while i < len(colours):
+    if(colours[3]=='purple'):
+        print(colours[3].upper())
+        i +=1
